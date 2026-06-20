bdcec62aaa00c3cdf1a9d8ea7d05d983145f7826
Vidhan 😎 <73152285+Vidhan-152@users.noreply.github.com>
2026-06-20 11:19:22 +0530

    fix: redirect to dashboard on login

--- src/main/java/com/vidhan152/playlistTracker/config/SecurityConfig.java
+++ src/main/java/com/vidhan152/playlistTracker/config/SecurityConfig.java
@@ -30,7 +30,7 @@ public class SecurityConfig {
                         .userInfoEndpoint(userInfo ->
                                 userInfo.userService(customOAuth2UserService)
                         )
-                        .defaultSuccessUrl("/api/auth/me", true)
+                        .defaultSuccessUrl("https://tracktube.vercel.app", true)
                 )
                 .csrf(csrf -> csrf.disable());
 
